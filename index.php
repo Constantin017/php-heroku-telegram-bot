164256123828dbc3079228ead4925f84d4d90910
Constantin017 <konstantin4all@gmail.com>
2018-12-02 05:27:37 +0200

    some php stuf index and telegram files

--- index.php
+++ index.php
@@ -1,2 +1,59 @@
 <?php
+require 'telegram.php';
 
+$bot_token = getenv('TELEGRAM_BOT_TOKEN');
+$app_name = getenv('HEROKU_APP_NAME');
+
+$uri = $_SERVER['REQUEST_URI'];
+switch($uri)
+{
+    case '/'.$bot_token:
+    {
+        $result = request();
+        $telegram->parse($result);
+        break;
+    }
+    case '/getWebhook':
+    {
+        $result = $telegram->getWebhookInfo();
+        dump($result);
+        break;
+    }
+    case '/setWebhook':
+    {
+        $bot_webhook = "https://" . $app_name . '.herokuapp.com/' . $bot_token;
+        $params = array(
+            'url' => $bot_webhook
+        );
+        $result = $telegram->setWebhook($params);
+        dump($result);
+        break;
+    }
+    case '/unsetWebhook':
+    {
+        $method = 'setWebhook';
+        $params = array(
+            'url' => null
+        );
+        $result = $telegram->setWebhook($params);
+        dump($result);
+        break;
+    }
+    default:
+    {
+        echo 'ok';
+    }
+}
+function request()
+{
+	$postdata = file_get_contents("php://input");
+	$json = json_decode($postdata, true);
+	if($json)
+		return $json;
+	return $postdata;
+}
+function dump($var){
+    echo "<pre>";
+    print_r($var);
+    echo "</pre>";
+}
--- telegram.php
+++ telegram.php
@@ -1,2 +1,31 @@
 <?php
+class telegram
+{
+    private $uri = 'https://api.telegram.org/bot';
+    private $bot_token;
 
+    public function __construct($bot_token)
+    {
+        $this->bot_token = $bot_token;
+    }
+
+    public function __call($name, $args)
+    {
+        return $this->call($name, count($args) === 0 ? [] : $args[0]);
+    }
+
+    public function call($method, $params = array())
+    {
+        $handle = curl_init($this->uri.$this->bot_token);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+        curl_setopt($handle, CURLOPT_POST, true);
+        curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($params));
+        curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+        $response = curl_exec($handle);
+
+        return ($response) ? json_decode($response, true) : false;
+    }
+
+}
